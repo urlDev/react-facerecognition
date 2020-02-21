13f06ef77648d7fead085887e12ce8c8556a4a80
Can Ural <can.ural@outlook.com>
2020-02-21 21:27:42 +0300

    finished making navigation, logo, rank, image link form components and added particles :star:

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import Navigation from "./components/navigation/Navigation.jsx";
+import Logo from "./components/logo/Logo.jsx";
+import ImageLinkForm from "./components/imageLinkForm/ImageLinkForm.jsx";
+import Rank from "./components/rank/Rank.jsx";
+
+import Particles from "react-particles-js";
+
+const particleOptions = {
+  particles: {
+    number: {
+      value: 200,
+      density: {
+        enable: true,
+        value_area: 800
+      }
+    }
+  }
+};
 
 function App() {
   return (
     <div className="App">
-      
+      <Particles className="particles" params={ particleOptions } />
+      <Navigation />
+      <Logo />
+      <Rank />
+      <ImageLinkForm />
+      {/* <FaceRecognition/> */}
     </div>
   );
 }
